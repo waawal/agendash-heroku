36b9818f3eeb424a92df6a9cd7f3bd61108fee8b
Daniel Waardal <daniel@massforstroel.se>
2017-09-25 15:21:22 +0200

    add application code

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+
+var express = require('express');
+var basicAuth = require('basic-auth-connect');
+var Agenda = require('agenda');
+var Agendash = require('agendash');
+
+const TITLE = process.env.TITLE || 'Agendash';
+var app = express();
+var agenda = new Agenda({db: {address: process.env.MONGODB_URI}});
+
+app.use(basicAuth(process.env.USER_NAME || 'admin', process.env.PASSWORD));
+app.use('/', Agendash(agenda, {middleware: 'express', title: TITLE}));
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
